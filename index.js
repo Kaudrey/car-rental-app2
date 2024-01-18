04ebaa60aa89bab0df0d254bacd8cb700a3e8041
Audrey-kirezi <audreykirezi100@gmail.com>
2024-01-18 16:42:00 +0200

    Connecting to database

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
-const express = require("express")
-const app = express()
-const dotenv = require('dotenv')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { Client } = require('pg');
 
-const userRoutes= require("./routes/user.routes")
-const postRoutes= require("./routes/post.routes")
-const swaggerUi= require ('swagger-ui-express')
-const swaggerJson = require('./swagger.json')
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const swaggerUi = require('swagger-ui-express');
+const swaggerJson = require('./swagger.json');
 
-dotenv.config({path: './.env'})
+dotenv.config({ path: './.env' });
 
 app.use(cors());
-app.use(express.json())
-app.use("/users",userRoutes)
-app.use("/posts",postRoutes)
-app.use("/swagger", swaggerUi.serve,swaggerUi.setup(swaggerJson))
-let PORT = process.env.PORT
-const mongoUsername = process.env.MONGO_USERNAME;
-const mongoPassword = process.env.MONGO_PASSWORD;
-const clusterName = process.env.MONGO_CLUSTER_NAME;
-const dbName = process.env.MONGO_DB_NAME;
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
-const encodedUsername = encodeURIComponent(mongoUsername);
+const client = new Client({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME, 
+});
 
-const DB_URL = `mongodb+srv://${encodedUsername}:${mongoPassword}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+client.connect()
+  .then(() => console.log("Connected to the database successfully"))
+  .catch(err => console.error("Error connecting to the database:", err));
 
-
-mongoose.connect(DB_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-
-.then(()=>console.log("Connected to database successfully"))
-.catch(err=>console.log(err))
-
-
-
-app.listen( PORT ,()=>{
-    console.log(`server is listening on port ${PORT}`);
-})
+let PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
